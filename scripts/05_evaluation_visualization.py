1647c28a5bf94af4e20c3ca3b81e218ca71d0183
BenjaminChan <benjamin.v.chan@gmail.com>
2025-04-25 12:28:26 -0700

    05_evaluation_visualization script plot_roc_curve and plot_clusters

--- scripts/05_evaluation_visualization.py
+++ scripts/05_evaluation_visualization.py
@@ -25,3 +25,34 @@ def plot_confusion_matrix(clf, X_test, y_test, output_dir):
     plt.savefig(os.path.join(output_dir, 'confusion_matrix.png'))
     plt.close()
 
+def plot_roc_curve(clf, X_test, y_test, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    y_score = clf.predict_proba(X_test)
+    classes = clf.classes_
+    for idx, cls in enumerate(classes):
+        fpr, tpr, _ = roc_curve(y_test == cls, y_score[:, idx])
+        roc_auc = auc(fpr, tpr)
+        plt.figure()
+        plt.plot(fpr, tpr, label=f'AUC = {roc_auc:.2f}')
+        plt.plot([0,1], [0,1], 'k--')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(f'ROC Curve for class {cls}')
+        plt.legend(loc='lower right')
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, f'roc_curve_class_{cls}.png'))
+        plt.close()
+
+def plot_clusters(kmeans, X, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    pca = PCA(n_components=2)
+    comps = pca.fit_transform(X)
+    plt.figure()
+    plt.scatter(comps[:,0], comps[:,1], c=kmeans.labels_, cmap='viridis')
+    plt.title('KMeans Clusters (PCA Projection)')
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, 'clusters_pca.png'))
+    plt.close()
+
